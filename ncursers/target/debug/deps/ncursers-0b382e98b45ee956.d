ad822a1b567af9a95fa5f08737c8da0c2fbf1e03
egginabox0 <98293106+egginabox0@users.noreply.github.com>
2025-08-08 14:14:40 -0500

    Delete ncursers/target directory
    istg this is not for the weak

--- ncursers/target/debug/deps/ncursers-0b382e98b45ee956.d
+++
@@ -1,7 +0,0 @@
-C:\Users\lucas\OneDrive\Desktop\rust and shit\ncursers\ncursers\target\debug\deps\ncursers-0b382e98b45ee956.d: src\lib.rs src\constants.rs src\window.rs
-
-C:\Users\lucas\OneDrive\Desktop\rust and shit\ncursers\ncursers\target\debug\deps\libncursers-0b382e98b45ee956.rmeta: src\lib.rs src\constants.rs src\window.rs
-
-src\lib.rs:
-src\constants.rs:
-src\window.rs:
